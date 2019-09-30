92b4f16f4f3dd6656fe3ca162d165631855fc8c5
luckyramdani007 <luckyramdani007@gmail.com>
2019-09-30 11:13:45 +0700

    add check verification code function

--- src/components/Verification.svelte
+++ src/components/Verification.svelte
@@ -3,33 +3,54 @@
     import CampaignOutput from '../routes/CampaignOutput/index';
 	import * as PDFGenerator from '../routes/CampaignOutput/PDFGenerator';
 	
-    var loading;
-
-    function Load() {
-	loading = setTimeout(showPage, 3000);
+	var loading;
+	var attempt = 3; 	
 	
-	document.getElementById("verification").style.display = "block";
-    document.getElementById("loader").style.display = "block";
-	document.getElementById("content").style.display = "none";
+	function verification() {
+		var password = document.getElementById("password").value;
+		if (password == "admin"){
+			loading = setTimeout(showPage, 3000);
+			document.getElementById("failed").style.display = "none";
+			document.getElementById("verification").style.display = "block";
+			document.getElementById("loader").style.display = "block";
+			document.getElementById("content").style.display = "none";
+			return false;
+		}
+		else{
+			attempt --;// Decrementing by one.
+			document.getElementById("failed").style.display = "block";
+			// Disabling fields after 3 attempts.
+		if( attempt == 0){
+			document.getElementById("submit").disabled = true;
+			return false;
+			}
+		}
 	}
 
     function showPage() {
 	document.getElementById("verification").style.display = "none";
+	document.getElementById("failed").style.display = "none";
 	document.getElementById("loader").style.display = "none";
 	document.getElementById("content").style.display = "block";
 	PDFGenerator.initPDFViewer("output.pdf");
 	}
+
 	export let action;
 </script>
 
 <div id="verification" class="row">
 	<div class="col-12 bg-danger mt-5 pt-5 " style="height: 100%; position: fixed;">
 		<div  id="container" class="mt-5 mb-5 pt-5" style="margin:0;">
-			<p class=" text-center text-light mt-5 pt-5">Easy_Campaign_Plus.pdf is protected. Please enter a Document Open Password</p>
-			<input  class="form-control mb-2" type="password" placehoder="Password">
-			<button on:click={Load} class="btn btn-block btn-secondary" >Submit</button>
-
-			<div id="loader" style="display:none;"></div>
+			<form id="validate" method="post">
+				<div id="failed" class="alert alert-danger" role="alert" style="display:none">
+					<strong>Verification code Wrong!</strong>
+				</div>
+				<p class=" text-center text-light mt-5 pt-5">Easy_Campaign_Plus.pdf is protected. Please enter a Document Open Password</p>
+				<input id="password" name="password" class="form-control mb-2" type="password" placehoder="Password">
+				<!-- <button on:click={Load} class="btn btn-block btn-secondary" >Submit</button> -->
+				<input type="button" class="btn btn-block btn-secondary" value="Submit" id="submit" on:click={verification}/>
+				<div id="loader" style="display:none;"></div>
+			</form>
 		</div>
 	</div>
 </div>
