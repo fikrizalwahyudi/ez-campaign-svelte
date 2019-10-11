7d1d4b46b295faa988ebe4883d6f9f2436556f88
luckyramdani007 <luckyramdani007@gmail.com>
2019-10-12 00:47:04 +0700

    fixing button komfirmasi ulang

--- src/routes/penawaran_setuju.svelte
+++ src/routes/penawaran_setuju.svelte
@@ -1,56 +1,52 @@
 <script>
-
+import { onMount } from 'svelte';
 import Input from '../../node_modules/sveltestrap/src/Input';
 import Button from '../../node_modules/sveltestrap/src/Button';
 import Navbar from '../../node_modules/sveltestrap/src/Navbar';
 
 export let action_setuju;
 
-    let verification = '';      
-    let countdown;
-    let countdown_number;
-
-    
-
-    function kirimulang() {
-        countdown_number = 6;
-        waiting();
-    }
-
-    function waiting() {
-        if(countdown_number > 0) {
-            countdown_number--;
-            document.getElementById('countdown_text').innerHTML = countdown_number;
-            if(countdown_number > 0) {
-                countdown = setTimeout( waiting, 1000);
+let verification = '';              
+function kirimulang(){
+        let timeoutHandle;
+        function countdown(minutes) {
+            let seconds = 5;
+            let mins = minutes;
+        function tick() {
+            let counter = document.getElementById("timer");
+            let current_minutes = mins-1
+            seconds--;
+            counter.innerHTML =
+            current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+            if( seconds > 0 ) {
+                timeoutHandle=setTimeout(tick, 1000);
                 document.getElementById("kirimulang").style.display = "none";
                 document.getElementById("waiting").style.display = "block";
-            }else{
+            } else {
                 document.getElementById("kirimulang").style.display = "block";
-                document.getElementById("waiting").style.display = "none"; 
+                document.getElementById("waiting").style.display = "none";
+                //    document.getElementById("kirimulang").onclick = countdown();
+
+                if(mins > 1){
+                setTimeout(function () { countdown(mins - 1); }, 1000);
+                }
             }
         }
-    }    
-
+        tick();
+    }
+    countdown(1);
+}
 </script>
 
     <div id="container" class="mt-5 mb-5 pt-5 animate-bottom text-center" >
-        
         <p class="text-center">Kode OTP (One Time Password) telah dikirimkan melalui telepon selular Anda</p>
         <p class="text-center">xxx-xxxx-1234</p>
-        <p class="text-center">berlaku:<span id="countdown_text"></span></p>
-                
-        <Button id="kirimulang" class="btn btn-danger btn-block mt-2" on:click={kirimulang} style="display:block">Kirim Ulang</Button>
+        <p class="text-center">berlaku: <span id="timer"></span></p>
+        <Input type="text" bind:value={verification} placeholder="Kode OTP" />   
+        <Button id="kirimulang" class="btn btn-danger btn-block mt-2" style="display:block" on:click={kirimulang} >Kirim Ulang</Button>
         <Button id="waiting" class="btn btn-secondary btn-block mt-2" style="display:none" disabled>Kirim Ulang</Button>
     </div>
-
-    <form method="post" id="roles" on:submit|preventDefault={action_setuju}>  
-        <Input type="text" bind:value={verification} placeholder="Kode OTP" />
-        <Navbar class="navbar bg-white fixed-bottom shadow-lg">
-            <input type="submit" class="btn btn-block btn-danger" disabled={!verification} value="Ajukan"/>
-        </Navbar>
-    </form>
-
-    
-
+    <Navbar class="navbar bg-white fixed-bottom shadow-lg">
+            <input type="submit" class="btn btn-block btn-danger" disabled={!verification} on:click={action_setuju} value="Ajukan"/>
+    </Navbar>
 
